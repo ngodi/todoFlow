5b99d19bafa4bffab69697b386c076bba8aa2fd2
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-10-18 00:28:03 -0500

    Todo controller refactored

--- src/todo_control.js
+++ src/todo_control.js
@@ -1,9 +1,24 @@
 import TodoList from './todo'
 
-const createTodo = (title, description) => {
-    
-};
+const todoController = (() => {
+    let todoListStorage = [];
+    let counter = 0;
 
-const storeTodo = (todo, todoList) => {
-    todoList.push(todo);
-};
+    const createTodo = (title, description, dueDate = null , priority = 'normal', notes = null, status = 'not started') => {
+        const toDo = new TodoList (counter++, title, description, dueDate, priority, notes, status);
+        storeTodo(toDo, todoListStorage);
+        return toDo;
+    };
+
+    const storeTodo = (todo, todoList) => {
+        todoList.push(todo);
+    };
+
+    const editTodo = (todoListStorage, title) => {
+
+    }
+    return { createTodo }
+})();
+
+export default todoController;
+
