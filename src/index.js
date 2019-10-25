d9cfcbd2cfe441f670b2187c81fd5ee017da317e
ngodi <albertngodi@gmail.com>
2019-10-25 21:24:39 +0100

    improved todo form method

--- src/index.js
+++ src/index.js
@@ -8,38 +8,6 @@ const todoListStorage = [];
 const projects = [];
 let counter = 0;
 
- 
-const createProject = (list) => {
- elements.newProject.addEventListener('click', projectForm); 
- elements.projectBtn.addEventListener('click', () => {
-    const newProject = todoForm();
-    list.push(newProject) ;
-    //displayProjects(projects);
-  });
-};
-
-const createTodo = (list) => {   
-    elements.todoBtn.addEventListener('click', () => {
-    const todos = getTodoInput();
-    const project = list[list.length - 1];
-    const toDo = new Todo(project, counter++, todos.title, todos.desc, todos.dueDate, todos.priority, todos.notes, todos.status);
-    list.push(toDo);
-  });
-};
-
-const displayProjects = (list) => {
- let projectOutput = `<ul class='projectList'>`;
-    list.forEach((element) => {
-        projectOutput += `<li id=${element}> ${element} </li>`;
-      });
-      projectOutput += `</ul>`;
-  uiController.domElementClass('projectList').innerHTML =  projectOutput;
-};
-
-const init = () => {
-    createProject(projects);
-    createTodo(todoListStorage);
-    //displayProjects(projects);
-  };
-
-init();
+elements.addTodoBtn.addEventListener('click', ()=>{
+ todoForm();
+});
