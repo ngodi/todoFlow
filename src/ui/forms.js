7205e98635bb049720c068631cfc9099cbce34eb
ngodi <albertngodi@gmail.com>
2019-10-25 21:19:50 +0100

    improved todo form method

--- src/ui/forms.js
+++ src/ui/forms.js
@@ -1,4 +1,5 @@
 import uiController from './ui_control';
+import elements from './dom_elements';
 
 export const getTodoInput = () =>{
   const title = uiController.domElementId('title').value;
@@ -15,10 +16,67 @@ export const getTodoInput = () =>{
 };
 
 export const todoForm = () => {
-  document.querySelector('#projectForm').classList.add('hidden');
-
-  const project = uiController.domElementId('project').value;
-  return project;
+ const markup = `
+              <div class='col-md-12 todoForm'>
+              <h3>Enter Todo Information</h3>
+              <div class='row'>
+               <div class='col-md-6'>
+                  <div class="form-group row">
+                     <label for="title" class="col-md-2">Title</label> 
+                     <div class="col-sm-10">
+                      <input type="text" class="form-control" id="title" placeholder="">
+                     </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="desc" class="col-md-2">Description</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="desc" placeholder="">
+                   </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="date" class="col-md-2">Due date</label>
+                    <div class="col-sm-10">
+                     <input type="date" class="form-control" id="date" placeholder="">
+                    </div>
+                  </div>
+              </div>
+              <div class='col-md-6'>
+                <div class="form-group row">
+                  <label for="priority" class="col-md-2">Priority</label>
+                  <div class="col-sm-10">
+                    <select class="form-control" id="priority">
+                      <option>Normal</option>
+                      <option>Low</option>
+                      <option>High</option>
+                    </select>
+                 </div>
+                </div>
+                <div class="form-group row">
+                  <label for="status" class="col-md-2">Status</label>
+                    <div class="col-sm-10">
+                     <select class="form-control" id="status">
+                      <option>New</option>
+                      <option>Pending</option>
+                      <option>Completed</option>
+                    </select>
+                    </div>
+                 </div>
+                 <div class="form-group row">
+                  <label for="notes" class="col-md-2">Notes</label>
+                  <div class="col-sm-10">
+                    <textarea class="form-control" id="notes" rows="1"></textarea>
+                  </div>
+                 </div>
+                </div>
+              </div>
+              <div class="form-group row">
+                 <div class='col-sm-7'></div>
+                 <div class="col-sm-5">
+                  <button type="button" class="btn btn-info btn-block" id="todoBtn" >Add to project</button>
+                </div>
+              </div>
+              </div>`;
+  elements.actionPanel.insertAdjacentHTML('beforeend', markup);
 };
 
 export const projectForm = () => {
