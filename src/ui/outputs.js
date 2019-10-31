f8a2210376f00cbc617af58fa28f43a7cdd8a038
ngodi <albertngodi@gmail.com>
2019-10-31 16:16:49 +0100

    refectored todo details edit

--- src/ui/outputs.js
+++ src/ui/outputs.js
@@ -28,19 +28,24 @@ export const setDropdown = () => {
   export const detailsDisplay = (todo) => {
     let markup = `<div class='col-md-12'>
                     <div class='row'>
-                      <div class='col-md-12 detailsItem'>
-                        <p class='blue-back'>${todo.title}<span class='deleteBtn' id='${todo.title}Delete'>X</span>
-                        </p>
-                        <p class='blue-back'>STATUS: ${todo.status}</p>
-                        <p class='blue-back'>PRIORITY: ${todo.priority}</p>
-                        <p class='blue-back'>DUE DATE: ${todo.dueDate}</p>
-                    </div>
-                    <div class='col-md-12'>
-                      <p class='center-text blue-back'>TASK DESCRIPTION</p>
-                      <p>${todo.description}</p>
+                      <div class='col-md-6'>
+                        <p class='blue-back active detailsItem'>${todo.title} Details<button class='btn btn-danger btn-sm deleteBtn' id='${todo.title}Delete'>X</button>
+                        </p>   
+                        
+                      <p class='blue-back detailsItem'>STATUS: ${todo.status}<button class='btn btn-danger btn-sm deleteBtn' id='${todo.status}'>change</button></p>                     
                       </div>
+                      <div class='col-md-6'>
+                        <p class='blue-back detailsItem'>PRIORITY: ${todo.priority}<button class='btn btn-danger btn-sm deleteBtn' id='${todo.priority}'>change</button></p>
+                        <p class='blue-back detailsItem'>DUE DATE: ${todo.dueDate}<button class='btn btn-danger btn-sm deleteBtn' id='${todo.dueDate}Delete'>change</button></p>
+                       </div>
                     </div>
-                 </div>`;
+                    <div class='row'>  
+                      <div class='col-md-12'>
+                       <p class='center-text  blue-back'>TASK DESCRIPTION</p>
+                       <p>${todo.description}<button class='btn btn-danger btn-xs deleteBtn' id='${todo.description}'>edit</button></p>
+                       </div>
+                    </div>
+                  </div>`;
     elements.detailsPanel.innerHTML = markup;
   };
 
@@ -49,7 +54,7 @@ export const setDropdown = () => {
   };
 
   export const showProjectHeading = (project) => {
-   elements.projectHeading.innerHTML = `${project} - Project Todo List <span class='deleteBtn' id='${project}Delete'>X</span>`;
+   elements.projectHeading.innerHTML = `${project} - Project Todo List <button class='btn btn-danger btn-xs deleteBtn' id='${project}Delete'>Delete</button>`;
    elements.todoBtn.innerHTML = `+ Add to ${project} Project`;
   };
  
